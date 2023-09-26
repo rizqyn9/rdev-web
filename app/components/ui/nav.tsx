b07826f72fd24041a50ae1371a23bdcab85bfa9f
Rizqy Nugroho <rizqynugroho9@gmail.com>
2023-09-26 12:59:51 +0700

    fix: project

--- app/components/ui/nav.tsx
+++ app/components/ui/nav.tsx
@@ -8,15 +8,15 @@ const NAV_ITEM = [
   },
   {
     label: "Blog",
-    to: "/a",
+    to: "#featured-project",
   },
   {
     label: "Work",
-    to: "/a",
+    to: "#featured-project",
   },
   {
     label: "About",
-    to: "/a",
+    to: "#featured-project",
   },
 ]
 
@@ -31,17 +31,20 @@ function NavRdev() {
 
 function NavList() {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-1 sm:gap-2 text-xs md:text-xl font-thin">
       {NAV_ITEM.map((item) => {
         return (
           <Link
             to={item.to}
             key={item.label}
-            className="group transition duration-300"
+            className="group transition duration-300 flex flex-col gap-0"
           >
-            <AnimatedTextTemplate1 text={item.label} className="px-2" />
+            <AnimatedTextTemplate1
+              text={item.label.toUpperCase()}
+              className="px-1 md:px-2"
+            />
             {/* <p className="px-2">{item.label}</p> */}
-            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-slate-500"></span>
+            <span className="block max-w-0 group-hover:max-w-full sm:-mt-2 transition-all duration-500 h-0.5 bg-slate-500"></span>
           </Link>
         )
       })}
@@ -62,7 +65,7 @@ function NavButton() {
 export function Nav() {
   return (
     <nav className="bg-slate-950 fixed top-0 z-50 left-0 right-0">
-      <div className="container px-4 flex justify-between py-4 items-center">
+      <div className="container px-4 flex justify-between pb-1 md:pb-4 pt-2 md:pt-7 items-center">
         <NavRdev />
         <NavList />
         {/* <NavButton /> */}
